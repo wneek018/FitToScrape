7eb70783792b0477a2695a204f2b053cd987e1be
Wendy Wigg <wiggwendy@gmail.com>
2019-07-02 15:58:54 -0700

    cleaning up

--- server.js
+++ server.js
@@ -36,55 +36,6 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 var routes = require("./controllers/index");
 app.use(routes);
 
-
-// // route for getting all Articles from the db
-// app.get("/articles", function(req, res) {
-//     //grab every document in the Articles collection
-//     db.Article.find({})
-//         .then(function(dbArticle) {
-//             //send found Articles back to the client
-//             res.json(dbArticle);
-//         })
-//         .catch(function(err) {
-//             //if error occurred, send to client
-//             res.json(err);
-//         });
-// });
-
-// // route for getting a specific Article by id, populate it with its note
-// app.get("/articles/:id", function(req, res) {
-//     //using id passed in the id parameter, find the matching one in the db
-//     db.Article.findOne({ _id: req.params.id })
-//         //populate all notes associated with it
-//         .populate("note")
-//         .then(function(dbArticle) {
-//             // if Article found, send back to the client
-//             res.json(dbArticle);
-//         })
-//         .catch(function(err) {
-//             // if error occurred, send back to the client
-//             res.json(err);
-//         });
-// });
-
-// // route for saving/updating an Article's associated Note
-// app.post("/articles/:id", function(req, res) {
-//     //create a new note and pass the req.body to the entry
-//     db.Note.create(req.body)
-//         .then(function(dbNote) {
-//             // if Note was created successfully, find one Article with the _id equal to req.params.id. Update the Article to be associated with the new Note
-//             return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-//         })
-//         .then(function(dbArticle) {
-//             // if successfully update an Article, send back to the client
-//             res.json(dbArticle);
-//         })
-//         .catch(function(err) {
-//             //if error occurred, send back to the client
-//             res.json(err);
-//         });
-// });     
-
 // start the server
 app.listen(PORT, function() {
     console.log("App running on port " + PORT);
